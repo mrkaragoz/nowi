f5a42cb6cabc85c590e006bb91e03814eb6514e7
Omer KARAGOZ <mrkaragoz@yahoo.com>
2023-07-11 12:58:09 +0300

    Added basic child support for menubar items

--- menubar.py
+++ menubar.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
 from pathlib import Path
-from typing import Callable, List, Tuple
+from typing import Callable, List, Tuple, Self
 from uuid import uuid4
 
 import pygame
@@ -18,15 +18,18 @@ class MenuBar:
 
     def __init__(self, height: int):
         self.height = height
+        file_menu: MenuItem = MenuItem(self, 0, "File", lambda: None)
+        file_menu.add_child(MenuItem(self, 0, "New Project", lambda: None))
+
         self.menu_items: List[MenuItem] = [
-            MenuItem(0, "File", height, lambda: None),
-            MenuItem(1, "Edit", height, lambda: None),
-            MenuItem(2, "Selection", height, lambda: None),
-            MenuItem(3, "View", height, lambda: None),
-            MenuItem(4, "Go", height, lambda: None),
-            MenuItem(5, "Run", height, lambda: None),
-            MenuItem(6, "Terminal", height, lambda: None),
-            MenuItem(7, "Help", height, lambda: None),
+            file_menu,
+            MenuItem(self, 1, "Edit", lambda: None),
+            MenuItem(self, 2, "Selection", lambda: None),
+            MenuItem(self, 3, "View", lambda: None),
+            MenuItem(self, 4, "Go", lambda: None),
+            MenuItem(self, 5, "Run", lambda: None),
+            MenuItem(self, 6, "Terminal", lambda: None),
+            MenuItem(self, 7, "Help", lambda: None),
         ]
         dist_from_left: int = 0
         for menu_item in sorted(self.menu_items, key=lambda x: x.order):
@@ -61,17 +64,17 @@ class MenuItem:
 
     label_padding: int = 8
 
-    def __init__(self, order, label: str, menubar_height, action: Callable):
+    def __init__(self, menubar: MenuBar, order: int, label: str, action: Callable):
         self.uuid: str = str(uuid4())
         self.order: int = order
         self.abs_coord_x: int = 0
         self.abs_coord_y: int = 0
         self.label: str = label
         self.highlighted: bool = False
-        self.menubar_height: int = menubar_height
+        self.menubar_height: int = menubar.height
         self.text_surf: pygame.Surface = pygame.font.SysFont(
             self.font_path.joinpath("ConsolaMono-Book.ttf").as_posix(),
-            int(menubar_height * 0.64),
+            int(self.menubar_height * 0.64),
             False,
         ).render(
             self.label,
@@ -81,6 +84,11 @@ class MenuItem:
         self.width: int = self.text_surf.get_width() + self.label_padding * 2
         self.label_height: int = self.text_surf.get_height()
         self.action: Callable = action
+        self.childs: List[Self] = []
+
+    def add_child(self, child: Self):
+        """Adds a chield to the menu item"""
+        self.childs.append(child)
 
     def draw(self) -> pygame.Surface:
         """Draws the menu item"""
